08ad7787baaca1c4ba1f54f700388181993ad0a4
Quodlun <115924991+Quodlun@users.noreply.github.com>
2025-02-13 18:18:05 +0800

    Project Rework

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,7 @@
 #include <Arduino.h>
-#include <toneAC.h>
-#include <NewPing.h>
-#include "define.h"
 
-/// @brief 距離感測器初始化
-NewPing sonar ( triggerPin, echoPin, maxDistance );
-
-/// @section 變數定義
-int distance = 0;
-
-/// @section Setup 程式
 void setup ()
-{
-  /// @brief 初始化序列埠, 鮑率: 9600
-  Serial.begin ( 9600 );
+{}
 
-  /// @brief 初始化元件腳位
-  pinMode ( ledPin, OUTPUT );
-}
-
-/// @section 主程式
 void loop ()
-{
-  /// @brief 距離感測器測距
-  unsigned int distance = sonar.ping_cm ();
-
-  /// @brief 偵測距離是否小於等於 10 公分
-  if ( distance <= 10 )
-  {
-    /// @brief 呼叫蜂鳴器副程式
-    sonarDetected ();
-    // ledDebug ();
-  }
-
-  /// @brief 顯示距離
-  Serial.print ( "距離(cm)：" );
-  Serial.println ( distance );
-}
+{}
